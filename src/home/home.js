cad8e0bc4345526e8c821bb53ef6c2085f8dc4bc
João <201701827@estudantes.ips.pt>
2024-09-14 23:01:07 +0100

    alteração home

---
+++ src/home/home.js
@@ -0,0 +1,14 @@
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Rapid Look</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default Home;
